4ef400979b6c5eed0a70a4d8ad7c6cfe0c77ece5
MuhammadS25 <mohammed20191700542@cis.asu.edu.eg>
2022-12-20 03:54:28 +0200

    Adding Dart Lists and methods

--- intro.dart
+++ intro.dart
@@ -16,4 +16,43 @@ void main() {
   bool isTrue = true; //true not 1
   isTrue = pi == 3.14; // true
   isTrue = pi == 3.14 ? true : false; // ternary operator
+
+  // lists
+  List<String> names = ['Mo', 'Ahmed', 'Ali']; // fixed length
+  names.add('Khaled'); // add to the end of the list
+  print(names);
+  names.insert(0, 'Mohamed'); // add to the beginning of the list
+  print(names);
+  names.removeAt(0); // remove from the beginning of the list
+  print(names);
+  names.removeLast(); // remove from the end of the list
+  print(names);
+  names[0]; // get the value of an index
+  names[0] = 'Mo'; // set the value of an index
+  names.remove('Ahmed'); // remove by value
+  print(names.contains('Mo')); // check if the list contains a value
+  print(names.indexOf('Mo')); // get the index of a value
+  print(names.length); // get the length of the list
+  print(names.isEmpty); // check if the list is empty
+  print(names.isNotEmpty); // check if the list is not empty
+  names.sort(); // sort the list
+  print(names);
+  names.reversed; // reverse the list
+  print(names.reversed);
+  names.forEach((name) => print(name)); // loop through the list
+  names.map((name) => name.toUpperCase()).forEach(
+      (name) => print(name)); // loop through the list and modify the values
+  names.where((name) => name.startsWith('M')).forEach(
+      (name) => print(name)); // loop through the list and filter the values
+  print(names.any(
+      (name) => name.startsWith('M'))); // check if any value starts with 'M'
+  print(names.every(
+      (name) => name.startsWith('M'))); // check if all values start with 'M'
+  print(names.firstWhere((name) =>
+      name.startsWith('M'))); // get the first value that starts with 'M'
+  print(names.lastWhere((name) =>
+      name.startsWith('M'))); // get the last value that starts with 'M'
+  names.join(', '); // join the list values with a comma and space
+  print(names);
+  names.clear(); // remove all elements
 }
