7b471f69fcf26404610a71a04051d3b1e80086d9
MuhammadS25 <mohammed20191700542@cis.asu.edu.eg>
2022-12-20 03:52:45 +0200

    Adding Dart Strings

--- intro.dart
+++ intro.dart
@@ -4,4 +4,11 @@ void main() {
   double pi = 3.14; // fractional numbers
   //print pi with 3 decimal places
   print(pi.toStringAsFixed(3));
+
+  // strings
+  String name = 'Mo';
+  print('Hello $name'); //accessing variables inside strings quotes
+  // string interpolation using curly braces
+  print(
+      'Hello ${name.toUpperCase()}'); //accessing methods inside strings quotes
 }
